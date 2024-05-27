574a5e962583aec0c91ef6b3a0d35b0ac5e0e08f
Pankaj Yadav <yadavpankaj28@gmail.com>
2024-05-27 22:04:03 +0530

    Initilialization of eureka server

---
+++ src/test/java/com/ecom/springeurekaserver/SpringEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.springeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
